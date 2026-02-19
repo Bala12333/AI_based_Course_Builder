34438d03bdf15161528eadaf7ec518703cba9e75
unknown <you@example.com>
2026-02-19 13:02:05 +0530

    Fix: Convert remaining API functions to CommonJS

--- api/get-courses.js
+++ api/get-courses.js
@@ -20,7 +20,7 @@ if (!admin.apps.length) {
 
 const db = admin.firestore();
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
     // Enable CORS
     res.setHeader('Access-Control-Allow-Credentials', true);
     res.setHeader('Access-Control-Allow-Origin', '*');
